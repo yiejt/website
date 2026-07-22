03ae102917e88e4ab09e3095747c4ab42c4c6f65
yiejt <yiejintian@gmail.com>
2026-07-22 19:38:06 +0800

    Update

---
+++ JavaScript/js.js
@@ -0,0 +1,69 @@
+const red_dot = document.querySelector(".collapsed-red-dot");
+const triangle = document.querySelector(".triangle");
+const red_dot_content = document.querySelector(".red-dot-content");
+
+document.addEventListener("click", function(){
+    triangle.classList.remove("show");
+    red_dot_content.classList.remove("show");
+});
+
+
+red_dot.addEventListener("click", function(e){
+    e.stopPropagation(); 
+    triangle.classList.toggle("show");
+    red_dot_content.classList.toggle("show");
+});
+
+red_dot_content.addEventListener("click", function(e){
+    e.stopPropagation(); 
+});
+triangle.addEventListener("click", function(e){
+    e.stopPropagation(); 
+});
+//
+
+const burger = document.querySelector(".burger");
+const burger_content = document.querySelector(".burger-nav")
+
+burger.addEventListener("click", () => {
+    burger.textContent =
+        burger.textContent === "menu" ? "close" : "menu";
+        
+    if (burger_content.classList.contains('show')) {
+        burger_content.classList.remove('show');
+    } else {
+        burger_content.classList.add('show');
+    }
+});
+//
+
+const header = document.querySelector("header");
+
+function updateHeaderHeight() {
+    const height = header.getBoundingClientRect().height;
+
+    document.documentElement.style.setProperty(
+        "--header-height",
+        `${height}px`
+    );
+
+    console.log(height);
+}
+
+updateHeaderHeight();
+
+window.addEventListener("resize", updateHeaderHeight);
+//
+
+const homepageImg =
+document.querySelector(".homepage-img");
+
+window.addEventListener("scroll",()=>{
+
+    const scroll =
+    window.scrollY;
+
+    homepageImg.style.transform =
+    `translateY(${scroll * 0.5}px)`;
+
+});
