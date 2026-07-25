b99ed15065eace2fee09bb7388712a70addb9a2f
yiejt <yiejintian@gmail.com>
2026-07-25 14:18:47 +0800

    update

--- JavaScript/js.js
+++ JavaScript/js.js
@@ -10,8 +10,8 @@ async function loadComponent(id, file){
     }
 }
 
-loadComponent("header","components/header.html");
-loadComponent("footer","components/footer.html");
+loadComponent("header","Components/header.html");
+loadComponent("footer","Components/footer.html");
 
 
 //Developing
